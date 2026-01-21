a37f985dbab45ca4cc915e09c8aaa04fe5bc7de4
B. Yao <106443070+oodame@users.noreply.github.com>
2026-01-21 15:00:23 +0800

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,9 +1,47 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      clangd = {
-        mason = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          mason = false,
+          cmd = {
+            "clangd",
+            "--background-index",
+            "-j=8",
+            "--clang-tidy",
+            "--all-scopes-completion",
+            "--completion-style=detailed",
+            "--header-insertion=never",
+            "--pch-storage=memory",
+            "--enable-config",
+            "--fallback-style=Google",
+            "--function-arg-placeholders",
+            "--header-insertion-decorators",
+            "--pretty",
+          },
+        },
+        zls = {
+          mason = false,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "vim",
+        "yaml",
+        "c",
+        "cpp",
+        "rust",
+        "zig",
       },
     },
   },
